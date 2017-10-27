c4acaa4e4e7b0870529baadb91165623a9cbf91f
oferyach <oferyach@gmail.com>
2017-10-27 23:34:13 +0300

    Init

---
+++ AuthTest/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AuthTest.LoyatlyService;
+
+
+namespace AuthTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+           InitializeComponent();
+           AuthTest.LoyatlyService.LoyaltyServiceClient s = new LoyaltyServiceClient();
+           AuthResult res = new AuthResult();
+           
+           
+           res = s.GetAuth("7215701085369600","3");
+           if (!res.Allowed)
+               MessageBox.Show("Denied: " + res.ErrorDesc);
+           else
+           
+
+            MessageBox.Show(res.DriverName+" "+res.ProductsList[0].ToString());
+
+        }
+    }
+}
---
+++ AuthWCFService/LoyaltyService.svc.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+
+namespace AuthService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service : LoyaltyService
+    {
+        public AuthResult GetAuth(string card,string stationcode)
+        {
+            AuthResult res = new AuthResult(); // { Allowed = true, DriverName = "Ofer" };
+
+            if (stationcode == "3")
+            {
+                res.Allowed = false;
+                res.ErrorDesc = "Not allowed in this station";
+                res.DriverName = "Ofer";
+                return res;
+            }
+
+            if (card == "7215701085369600")
+            {
+                res.Allowed = true;
+                res.DriverName = "Ofer";
+                res.Discount = 5.0;
+                res.Limit = 5000;
+
+                res.Reference = "ABCD";
+
+                
+
+                res.ProductsList.Add(new ProductItem());
+
+                res.ProductsList[0].Code = 103;
+                res.ProductsList[0].Discount = 5;
+                res.ProductsList[0].DiscountType = "Abs";
+
+                res.ProductsList.Add(new ProductItem());
+
+                res.ProductsList[1].Code = 104;
+                res.ProductsList[1].Discount = 5;
+                res.ProductsList[1].DiscountType = "%";
+
+
+                
+            }
+            else
+            {
+                res.Allowed = false;
+                res.ErrorDesc = "Unknow card.";
+                res.DriverName = "Lionel";
+            }
+            return res;
+        }
+
+        public TransactionCompleteResult TransactionComplete(string referece, double amount, double volume, int ProductCode,DateTime datetime)
+        {
+            TransactionCompleteResult res = new TransactionCompleteResult();
+
+            res.Recived = true;
+            res.ErrorDesc = "OK";
+            
+            return res;
+        }
+
+       
+    }
+}
